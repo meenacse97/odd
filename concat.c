2848ef03ac88771373f6243435209486eeec344e
meenacseengineering5 <meenacse1997@gmail.com>
2016-12-22 12:08:34 +0530

    Add files via upload

---
+++ concat.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+char a[100],b[100];
+printf("enter the first and second string:");
+gets(a);
+gets(b);
+printf("\n the string concadination is:%s",a,b);
+}
